b87a21b32c7b9d507d07488da5d1aaffa56934fc
Ioannis Gabrielides <ioannisgabrielides@gmail.com>
2017-10-07 12:44:02 +0100

    Add 2 example images and Link to GitHub

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { /*title: 'Ping Me'*/ });
+  res.render('index', { title: 'PingMe' });
 });
 
 module.exports = router;
